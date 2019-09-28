ec8892a3e484132e605b062aae385a1811e2b189
Bvekv1 <bvekp1@gmail.com>
2019-09-28 14:47:51 +0545

    Add, Del, Subtract , Divide , Del button completed

--- app/src/main/java/com/calculator/cal/MainActivity.java
+++ app/src/main/java/com/calculator/cal/MainActivity.java
@@ -3,24 +3,30 @@ package com.calculator.cal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editText;
-    private Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnAdd, btnSub, btnDivide, btnPercentage, btnEqual;
+    private TextView editText;
+   double input1 = 0 , input2 =0;
+    boolean Addition, Subtract, Division, decimal;
+    private Button btnDel, btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnAdd, btnSub, btnDivide, btnPercentage, btnEqual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Binding
+        btnDel = findViewById(R.id.btnDel);
         editText = findViewById(R.id.etText);
         btnAdd = findViewById(R.id.btnAdd);
         btnDivide = findViewById(R.id.btnDivide);
         btnSub = findViewById(R.id.btnSub);
         btnEqual = findViewById(R.id.btnEqual);
         btnPercentage = findViewById(R.id.btnPercentage);
+        btnZero = findViewById(R.id.btnZero);
         btnOne = findViewById(R.id.btnOne);
         btnTwo = findViewById(R.id.btnTwo);
         btnThree = findViewById(R.id.btnThree);
@@ -30,6 +36,140 @@ public class MainActivity extends AppCompatActivity {
         btnSeven = findViewById(R.id.btnSeven);
         btnEight = findViewById(R.id.btnEight);
         btnNine = findViewById(R.id.btnNine);
+
+
+
+        //Setting Custom Value To Android
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "0");
+            }
+        });
+
+       btnOne.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+                editText.setText(editText.getText() + "1");
+
+           }
+       });
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "2");
+            }
+        });
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "3");
+            }
+        });
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "4");
+            }
+        });
+        btnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "5");
+            }
+        });
+        btnSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "6");
+            }
+        });
+        btnSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "7");
+            }
+        });
+        btnEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "8");
+            }
+        });
+        btnNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText() + "9");
+            }
+        });
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText.getText().length() !=0){
+                    input1 = Float.parseFloat(editText.getText() + "");
+                    Addition = true;
+                    decimal =false;
+                    editText.setText(null);
+                }
+            }
+        });
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText.getText().length() !=0){
+                    input1 = Float.parseFloat(editText.getText() + "");
+                    Subtract = true;
+                    decimal =false;
+                    editText.setText(null);
+                }
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText.getText().length() !=0){
+                    input1 = Float.parseFloat(editText.getText() + "");
+                    Division = true;
+                    decimal = false;
+                    editText.setText(null);
+
+                }
+            }
+        });
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Addition || Subtract || Division ){
+                      input2 = Float.parseFloat(editText.getText() + "");
+                }
+                if (Addition){
+                    editText.setText(input1 + input2 + "");
+                    Addition = false;
+                }
+                if (Subtract){
+                    editText.setText(input1 - input2 + "");
+                    Subtract = false;
+                }
+                if(Division){
+                    editText.setText(input1 / input2 + "");
+                    Division = false;
+                }
+            }
+        });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+                input1 = 0.0;
+                input2 = 0.0;
+            }
+        });
+
 
 
     }
